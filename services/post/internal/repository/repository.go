058f661c4b4575acdb3e1a0283faba646867985c
londonwaterloo <superkran7@gmail.com>
2026-05-15 12:05:13 +0300

    fix: demo data

--- services/post/internal/repository/repository.go
+++ services/post/internal/repository/repository.go
@@ -78,6 +78,9 @@ func (s *postStorage) Save(ctx context.Context, post model.Post) (int64, error) 
 
 func (s *postStorage) Delete(ctx context.Context, id int64) error {
 	start := time.Now()
+	if _, err := s.db.Exec(ctx, `DELETE FROM like_record WHERE post_id=$1 OR comment_id IN (SELECT id FROM comment WHERE post_id=$1)`, id); err != nil {
+		return err
+	}
 	tag, err := s.db.Exec(ctx, `DELETE FROM post WHERE id=$1`, id)
 	logQuery(ctx, "postStorage.Delete", start)
 	if err != nil {
